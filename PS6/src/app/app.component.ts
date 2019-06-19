fe6c00fd20c2e3a852c4da0ad3d1337e5eb4f4c2
Zhilin Guo <gzl2017@bu.edu>
2019-06-18 23:55:56 -0400

    Readability update

--- PS6/src/app/app.component.ts
+++ PS6/src/app/app.component.ts
@@ -11,6 +11,7 @@ import { GetRecentlyPlayedGamesAsyncService } from './services/RecentlyGames-asy
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
+  // initializing variables
   title = "CS591 PS6 - Search For User's Recently Played Games";
   SteamID: number;
 
@@ -19,7 +20,7 @@ export class AppComponent implements OnInit{
 
   currentRecentlyPlayedGames: RecentlyPlayedGame[];
 
-
+  // method to call service and retrieve recently played games
   submitSteamID(): void {
     let inputSteamID: number = this.SteamID;
     this.recentGamesService.getRecentlyPlayedGames(inputSteamID)
